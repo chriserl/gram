67ba63f7de977eb48ac145d19f0b98b69d253919
chriserl <chriserldev@gmail.com>
2020-06-17 15:29:07 +0000

    finished creating side navigation. also added javascript functionality

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 const aside = document.querySelector("aside");
 const hamBtn = document.querySelector(".hamBtn");
+const closeHamBtn = document.querySelector(".close-ham");
+const sideLinks = document.querySelectorAll(".menu-item");
 
 const toggleAside = () => {
   aside.classList.contains("profile-inactive")
@@ -7,4 +9,22 @@ const toggleAside = () => {
     : aside.classList.add("profile-inactive");
 };
 
-hamBtn.addEventListener("click", () => toggleAside());
+const rmActiveLink = () => {
+  sideLinks.forEach((sideLink) => {
+    sideLink.classList.contains("item-active") &&
+      sideLink.classList.remove("item-active");
+  });
+};
+
+const addActiveLink = (linkItem) => {
+  linkItem.classList.add("item-active");
+};
+
+hamBtn.addEventListener("click", toggleAside);
+closeHamBtn.addEventListener("click", toggleAside);
+sideLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    rmActiveLink();
+    addActiveLink(link);
+  });
+});
