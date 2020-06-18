eed3bcdcd87c6df4ff626ac3dd882f9edc116c17
chriserl <chriserldev@gmail.com>
2020-06-18 20:43:58 +0000

    commented javascript code

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
-const aside = document.querySelector("aside");
-const hamBtn = document.querySelector(".hamBtn");
-const closeHamBtn = document.querySelector(".close-ham");
-const sideLinks = document.querySelectorAll(".menu-item");
+const aside = document.querySelector("aside"); //aside element
+const hamBtn = document.querySelector(".hamBtn"); //hamburger icon
+const closeHamBtn = document.querySelector(".close-ham"); //close aside button
+const sideLinks = document.querySelectorAll(".menu-item"); //links in aside
 
+//toggle aside
 const toggleAside = () => {
   aside.classList.contains("profile-inactive")
     ? aside.classList.remove("profile-inactive")
     : aside.classList.add("profile-inactive");
 };
 
+//remove active link in aside
 const rmActiveLink = () => {
   sideLinks.forEach((sideLink) => {
     sideLink.classList.contains("item-active") &&
@@ -16,12 +18,16 @@ const rmActiveLink = () => {
   });
 };
 
+//add active link class to aside link
 const addActiveLink = (linkItem) => {
   linkItem.classList.add("item-active");
 };
 
-hamBtn.addEventListener("click", toggleAside);
-closeHamBtn.addEventListener("click", toggleAside);
+//Event listeners
+hamBtn.addEventListener("click", toggleAside); //toggle aside listener
+closeHamBtn.addEventListener("click", toggleAside); //close aside listener
+
+//aside links listener
 sideLinks.forEach((link) => {
   link.addEventListener("click", () => {
     rmActiveLink();
