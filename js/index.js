1c6b6b1cbbbc01a8780d01504861f15256a5cdbe
chriserl <chriserldev@gmail.com>
2020-06-19 11:39:22 +0000

    wrote javascript to slice post user link length

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ const aside = document.querySelector("aside"); //aside element
 const hamBtn = document.querySelector(".hamBtn"); //hamburger icon
 const closeHamBtn = document.querySelector(".close-ham"); //close aside button
 const sideLinks = document.querySelectorAll(".menu-item"); //links in aside
+const postlinks = document.querySelectorAll(".post-user-link");
+const postLinkChars = 9; //maximum characters for post links
 
 //toggle aside
 const toggleAside = () => {
@@ -22,6 +24,16 @@ const rmActiveLink = () => {
 const addActiveLink = (linkItem) => {
   linkItem.classList.add("item-active");
 };
+
+//Slice link character length
+(function () {
+  postlinks.forEach((postLink) => {
+    let link = postLink.innerHTML;
+    const linkLength = link.length;
+    linkLength > postLinkChars && (link = link.slice(0, postLinkChars) + "...");
+    postLink.innerHTML = link;
+  });
+})();
 
 //Event listeners
 hamBtn.addEventListener("click", toggleAside); //toggle aside listener
