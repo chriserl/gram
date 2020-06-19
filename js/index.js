f9624fd0129c52e7588280dd466c5a0ebb154dde
chriserl <chriserldev@gmail.com>
2020-06-19 19:49:19 +0000

    added expansion functionality to post

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ const hamBtn = document.querySelector(".hamBtn"); //hamburger icon
 const closeHamBtn = document.querySelector(".close-ham"); //close aside button
 const sideLinks = document.querySelectorAll(".menu-item"); //links in aside
 const postlinks = document.querySelectorAll(".post-user-link");
+const feedImage = document.querySelector(".feed-image"); //post details button
+const feedInfo = document.querySelector(".feed-info"); //feed details
 const postLinkChars = 9; //maximum characters for post links
 
 //toggle aside
@@ -25,6 +27,13 @@ const addActiveLink = (linkItem) => {
   linkItem.classList.add("item-active");
 };
 
+//toggle post details
+const togglePostDetails = () => {
+  feedInfo.classList.contains("hidden")
+    ? feedInfo.classList.remove("hidden")
+    : feedInfo.classList.add("hidden");
+};
+
 //Slice link character length
 (function () {
   postlinks.forEach((postLink) => {
@@ -38,6 +47,7 @@ const addActiveLink = (linkItem) => {
 //Event listeners
 hamBtn.addEventListener("click", toggleAside); //toggle aside listener
 closeHamBtn.addEventListener("click", toggleAside); //close aside listener
+feedImage.addEventListener("click", togglePostDetails); //toggle post details
 
 //aside links listener
 sideLinks.forEach((link) => {
